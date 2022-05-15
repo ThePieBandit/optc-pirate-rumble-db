21662710108a7b0d9e0a7c4446dd32bb4fde938f
André Mantas <andremantas7@gmail.com>
2022-05-15 17:30:49 +0100

    Add logic to keep looking for basedOn unit details

--- src/app/core/services/unit.service.ts
+++ src/app/core/services/unit.service.ts
@@ -76,12 +76,19 @@ class UserService {
     const units: rumble.Entry[] = rumbleData.units as rumble.Entry[];
     for (let i = 0; i < units.length; i++) {
       let unit: rumble.Unit;
-      if ('basedOn' in units[i]) {
-        const baseUnit: rumble.Unit = units.find(u => u.id === (units[i] as rumble.Reference).basedOn) as rumble.Unit;
+      let basedOn = units[i].basedOn;
+      if (basedOn) {
+        let baseUnit: rumble.Unit;
+        do {
+          baseUnit = units.find(u => u.id === basedOn) as rumble.Unit;
+          basedOn = baseUnit && baseUnit.basedOn;
+        } while (basedOn);
+
         if (baseUnit == null) {
           console.log( ' Failed to locate Base Unit!!!!!!! ' + i, units[i]);
           continue;
         }
+
         try {
           unit = JSON.parse(JSON.stringify(baseUnit));
         } catch (error) {
